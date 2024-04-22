36145c13c9a8f06301d56322393cb65e36ef9235
taviya lalji <taviya2003@gmail.com>
2024-04-22 22:16:07 +0530

    ref hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import UseEffectHook from './component/UseEffect'
+import UserefHook from './component/useRef'
 
 function App() {
   return(
     <>
     {/* <UseEffectHook /> */}
+    <UserefHook />
     </>
   )
 }
--- src/component/useRef.jsx
+++ src/component/useRef.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState,useRef} from 'react'
 
 function UserefHook(){
+    const [input, setInput]= useState("")
+    const RefValue = useRef(0) 
+
+    useEffect(()=>{
+        RefValue.current = RefValue.current + 1;
+    });
+
     return(
         <>
+        <input type="text" value={input}  onChange={(e)=>{setInput(e.target.value)}}/>
+        <button>click</button>
+        {/* {input} */}
+        <div>No of word : {RefValue.current}</div>
         </>
     );
 };
